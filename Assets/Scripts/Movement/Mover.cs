8f33773eb9d26d9753668b65c3b116681230522d
Martin Kolev <martinkolevbg93@gmail.com>
2019-11-21 04:24:54 +0200

    Started building combat system and made dependency structure for the classes.

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -3,34 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Mover : MonoBehaviour
+namespace RPG.Movement
 {
+    public class Mover : MonoBehaviour
+    {
 
-    [SerializeField] Transform target;
+        [SerializeField] Transform target;
 
-    void Update()
-    {
-        if (Input.GetMouseButton(0))
+        void Update()
         {
-            MoveToCursour();
+            UpdateAnimator();
         }
-        UpdateAnimator();
-    }
-    void MoveToCursour()
-    {
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        bool hasHit = Physics.Raycast(ray, out hit);
-        if(hasHit)
+
+        public void MoveTo(Vector3 destination)
         {
-            GetComponent<NavMeshAgent>().destination = hit.point;
+            GetComponent<NavMeshAgent>().destination = destination;
         }
-    }
-    private void UpdateAnimator()
-    {
-        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
-        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
-        float speed = localVelocity.z;
-        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+
+        private void UpdateAnimator()
+        {
+            Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+            Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+            float speed = localVelocity.z;
+            GetComponent<Animator>().SetFloat("forwardSpeed", speed);
+        }
     }
 }
