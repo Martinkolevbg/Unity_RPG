72fe6c0d243e6f8bed88b7c8ba6af4f9537d94a5
Martin Kolev <martinkolevbg93@gmail.com>
2019-11-30 14:58:58 +0200

    Created floater script

---
+++ Assets/Scripts/Floater.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floater : MonoBehaviour
+{
+
+    [SerializeField] float rotationSpeed = 3.0f;
+    [SerializeField] float tolerance = 0.5f;
+    [SerializeField] float frequency = 0.5f;
+
+    Vector3 posOffset = new Vector3();
+    Vector3 tempPos = new Vector3();
+
+    void Start()
+    {
+        posOffset = transform.position;
+    }
+    void Update()
+    {
+        RotateObject();
+
+        UpDownFloating();
+    }
+
+    private void UpDownFloating()
+    {
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * tolerance;
+
+        transform.position = tempPos;
+    }
+
+    private void RotateObject()
+    {
+        transform.Rotate(new Vector3(0f, Time.deltaTime * rotationSpeed, 0f), Space.World);
+    }
+}
