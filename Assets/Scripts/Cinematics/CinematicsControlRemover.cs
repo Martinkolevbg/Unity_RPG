689a1d048a7435d0f76167c0100240551a321c44
Martin Kolev <martinkolevbg93@gmail.com>
2019-12-07 16:45:40 +0200

    Added cutscene trigger.

---
+++ Assets/Scripts/Cinematics/CinematicsControlRemover.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace RPG.Cinematics
+{
+    public class CinematicsControlRemover : MonoBehaviour 
+    {
+        private void Start() 
+        {
+            
+        }
+
+        void DIsableControl()
+        {
+            print("Control DISABLED");
+        }
+
+        void EnableControl()
+        {
+            print("Control ENABLED");
+        }
+        
+    }
+}
