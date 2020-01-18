984a624bb9a45958b664beb5aaa849e55a67ef35
Martin Kolev <martinkolevbg93@gmail.com>
2020-01-18 14:04:39 +0200

    Added left handed weapon Bow and arrows(projectiles).

--- Assets/Scripts/Control/Combat/Fighter.cs
+++ Assets/Scripts/Control/Combat/Fighter.cs
@@ -8,7 +8,8 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float timeBetweenAttacks = 1f;
-        [SerializeField] Transform handTransform = null;
+        [SerializeField] Transform rightHandTransform = null;
+        [SerializeField] Transform leftHandTransform = null;
         [SerializeField] Weapon defaultWeapon = null;
 
         Health target;
@@ -54,8 +55,24 @@ namespace RPG.Combat
         void Hit()
         {
             if (target == null) { return; }
+
+            if (currentWeapon.HasProjectile())
+            {
+                currentWeapon.LaunchProjectile(rightHandTransform, leftHandTransform, target);
+            }
+            else
+            {
+                target.TakeDamage(currentWeapon.GetDamage());
+            }
+
             target.TakeDamage(currentWeapon.GetDamage());
         }
+
+        void Shoot()
+        {
+            Hit();
+        }
+
         private bool GetIsInRange()
         {
             return Vector3.Distance(transform.position, target.transform.position) < currentWeapon.GetRange();
@@ -88,7 +105,7 @@ namespace RPG.Combat
         {
             currentWeapon = weapon;
             Animator animator = GetComponent<Animator>();
-            weapon.Spawn(handTransform , animator);
+            weapon.Spawn(rightHandTransform , leftHandTransform , animator);
         }
     }
 }
--- Assets/Scripts/Control/Combat/Weapon.cs
+++ Assets/Scripts/Control/Combat/Weapon.cs
@@ -1,30 +1,55 @@
-﻿using UnityEngine;
+﻿using RPG.Core;
+using UnityEngine;
 
 namespace RPG.Combat
 {
-    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons / Make new Weapon", order = 0)]
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Weapons/Make New Weapon", order = 0)]
     public class Weapon : ScriptableObject
     {
-        [SerializeField] AnimatorOverrideController weaponAnimatorOverride = null;
-        [SerializeField] GameObject weaponPrefab = null;
+        [SerializeField] AnimatorOverrideController animatorOverride = null;
+        [SerializeField] GameObject equippedPrefab = null;
         [SerializeField] float weaponDamage = 5f;
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] bool isRightHanded = true;
+        [SerializeField] Projectile projectile = null;
 
-        public void Spawn(Transform handTransform, Animator animator)
+        public void Spawn(Transform rightHand, Transform leftHand, Animator animator)
         {
-            if(weaponPrefab != null)
+            if (equippedPrefab != null)
             {
-                Instantiate(weaponPrefab,handTransform);      
+                Transform handTransform = GetTransform(rightHand, leftHand);
+                Instantiate(equippedPrefab, handTransform);
             }
-            if(weaponAnimatorOverride != null)
+            if (animatorOverride != null)
             {
-                animator.runtimeAnimatorController = weaponAnimatorOverride;
+                animator.runtimeAnimatorController = animatorOverride;
             }
         }
+
+        private Transform GetTransform(Transform rightHand, Transform leftHand)
+        {
+            Transform handTransform;
+            if (isRightHanded) handTransform = rightHand;
+            else handTransform = leftHand;
+            return handTransform;
+        }
+
+        public bool HasProjectile()
+        {
+            return projectile != null;
+        }
+
+        public void LaunchProjectile(Transform rightHand, Transform leftHand, Health target)
+        {
+            Projectile projectileInstance = Instantiate(projectile, GetTransform(rightHand, leftHand).position, Quaternion.identity);
+            projectileInstance.SetTarget(target);
+        }
+
         public float GetDamage()
         {
             return weaponDamage;
         }
+
         public float GetRange()
         {
             return weaponRange;
