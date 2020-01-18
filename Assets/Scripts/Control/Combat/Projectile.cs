cca56e0dee27c9756d0d9caee96d863239e03ca2
Martin Kolev <martinkolevbg93@gmail.com>
2020-01-18 22:52:06 +0200

    Added Prefabs of weapons & projectiles. Created new type of enemy.

--- Assets/Scripts/Control/Combat/Projectile.cs
+++ Assets/Scripts/Control/Combat/Projectile.cs
@@ -3,32 +3,41 @@ using System.Collections.Generic;
 using RPG.Core;
 using UnityEngine;
 
-
 public class Projectile : MonoBehaviour
 {
+    [SerializeField] float speed = 1;
     Health target = null;
-    [SerializeField] float speed;
+    float damage = 0;
 
     void Update()
     {
         if (target == null) return;
+
         transform.LookAt(GetAimLocation());
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
 
-    public void SetTarget(Health target)
+    public void SetTarget(Health target, float damage)
     {
         this.target = target;
+        this.damage = damage;
     }
-
 
     private Vector3 GetAimLocation()
     {
         CapsuleCollider targetCapsule = target.GetComponent<CapsuleCollider>();
-        if(targetCapsule == null)
+        if (targetCapsule == null)
         {
             return target.transform.position;
-        }    
+        }
         return target.transform.position + Vector3.up * targetCapsule.height / 2;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Health>() != target) return;
+        target.TakeDamage(damage);
+        Destroy(gameObject);
+    }
+
 }
